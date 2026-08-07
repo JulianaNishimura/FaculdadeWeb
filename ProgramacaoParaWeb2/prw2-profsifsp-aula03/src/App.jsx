c4f612f2cbb5c72d9ccbac0c7bfc76c961a7cb2f
unknown <juliananishimura237@gmail.com>
2026-08-06 23:21:20 -0300

    aula 3 - iniciando um projeto

---
+++ ProgramacaoParaWeb2/prw2-profsifsp-aula03/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './components/banner/Banner'
+import Formulario from './components/formulario/Formulario'
+
+function App() {
+
+  return (
+    <div>
+      <Banner/>
+      <Formulario/>
+    </div>
+  )
+}
+
+export default App
---
+++ ProgramacaoParaWeb2/prw2-profsifsp-aula03/src/components/banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner(){
+    return (
+    <header className="banner">
+        <img src="images/bannerifsp.png" alt="imagem aerea do ifsp" />
+    </header>
+    )
+}
+
+export default Banner;
